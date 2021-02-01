10d8222b670f761c59e78c7a05a928c5c7451224
Brian Deckard <bdeckard@gmail.com>
2021-02-01 10:55:00 -0800

    Add project files.

---
+++ MySignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MySignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public ChatHub()
+        {
+        }
+
+        public void Send( string user, string message )
+        {
+            Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
